3138ac59e58a3576e1839e0648cc8a5ecf6d195a
puay12 <puayyssii12@gmail.com>
2022-02-26 16:15:54 +0700

    init

--- Prak1/no1.py
+++ Prak1/no1.py
@@ -1,2 +1,14 @@
-import cv2;
-import numpy;
+import cv2
+
+#membaca image
+img = cv2.imread("bg.jpg")
+
+#menampilkan ukuran dan struktur data image
+print("Ukuran gambar : ", img.shape)
+print("Type data : ", img.dtype)
+
+#menampilkan image
+cv2.imshow("Image : ", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
